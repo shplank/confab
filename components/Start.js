fd7cdebc7574b05b400677445184550ae4e96f25
shplank <lankyjoe@gmail.com>
2021-11-03 11:08:21 -0700

    initial Start page styling

--- components/Start.js
+++ components/Start.js
@@ -1,7 +1,7 @@
 // The application’s start screen that loads and renders first
 
 import React from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { StyleSheet, View, ImageBackground, Text, TextInput, Button } from 'react-native';
 
 export default class Start extends React.Component {
   constructor(props) {
@@ -11,22 +11,122 @@ export default class Start extends React.Component {
 
   render() {
     return (
-      <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{fontSize: 45, fontWeight: 600, fontColor: '#FFFFFF'}}>confab</Text>
-        <View>
-          <TextInput
-            style={{height: 40, borderColor: 'black', borderWidth: 1, alignSelf: 'center', padding: 10}}
-            onChangeText={(name) => this.setState({name})}
-            value={this.state.name}
-            placeholder="what's your name?"
-          />
-          <Button
-            title="start chatting"
-            style={{fontSize: 16, fontWeight: 600, fontColor: '#FFFFFF', buttonColor: '#757083'}}
-            onPress={() => this.props.navigation.navigate('Chat', { name: this.state.name  })}
-          />
-        </View>
+      <View style={styles.container}>
+        <ImageBackground source={require("../BackgroundImage.png")} resizeMode="cover" style={styles.bgImage}>
+          <Text style={styles.title}>confab</Text>
+          <View style={styles.startBox}>
+            <TextInput
+              style={styles.nameInput}
+              onChangeText={(name) => this.setState({name})}
+              value={this.state.name}
+              placeholder="your name"
+            />
+            <View style={styles.bgChoice}>
+              <Text style={styles.chooseText}>choose background color:</Text>
+              <View style={styles.bgChoices}>
+                <View title="" style={styles.bgColor1} />
+                <View title="" style={styles.bgColor2} />
+                <View title="" style={styles.bgColor3} />
+                <View title="" style={styles.bgColor4} />
+              </View>
+            </View>
+            <Button
+              title="start chatting"
+              style={styles.startButton}
+              onPress={() => this.props.navigation.navigate('Chat', { name: this.state.name  })}
+            />
+          </View>
+        </ImageBackground>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-evenly', 
+    alignItems: 'center'
+  },
+  bgImage: {
+    flex: 1, 
+    width: '100%', 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+  title: {
+    flex: 1,
+    fontSize: 45, 
+    fontWeight: '600', 
+    color: '#FFFFFF', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    textAlign: 'center'
+  },
+  startBox: {
+    flex: 0.44, 
+    width: '88%', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    backgroundColor: '#FFFFFF'
+  },
+  nameInput: {
+    height: 40, 
+    width: '88%', 
+    borderColor: 'black', 
+    borderWidth: 1, 
+    alignSelf: 'center', 
+    color:'#757083', 
+    opacity: 50, 
+    padding: 10
+  },
+  bgChoice: {
+    width: '88%', 
+    flexDirection: 'column', 
+    alignItems: 'center'
+  },
+  chooseText: {
+    fontSize: 16, 
+    fontWeight: '300', 
+    color: '#757083', 
+    opacity: 100, 
+    justifyContent: 'center', 
+    textAlign: 'left'
+  },
+  bgChoices: {
+    flexDirection: 'row', 
+    justifyContent: 'space-evenly'
+  },
+  bgColor1: {
+    height: 60, 
+    width: 60, 
+    borderRadius: 30, 
+    backgroundColor: '#090C08'
+  },
+  bgColor2: {
+    height: 60, 
+    width: 60, 
+    borderRadius: 30, 
+    backgroundColor: '#474056'
+  },
+  bgColor3: {
+    height: 60, 
+    width: 60, 
+    borderRadius: 30, 
+    backgroundColor: '#8A95A5'
+  },
+  bgColor4: {
+    height: 60, 
+    width: 60, 
+    borderRadius: 30, 
+    backgroundColor: '#B9C6AE'
+  },
+  startButton: {
+    width: '88%', 
+    fontSize: 16, 
+    fontWeight: '600', 
+    color: '#FFFFFF', 
+    backgroundColor: '#757083'
+  }
+});
