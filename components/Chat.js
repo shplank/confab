7327008bd6b475da4a50e1986c7d9055f567d707
shplank <lankyjoe@gmail.com>
2021-11-08 16:38:00 -0800

    fixed database update

--- components/Chat.js
+++ components/Chat.js
@@ -47,6 +47,7 @@ export default class Chat extends React.Component {
     let { name } = this.props.route.params;
     this.props.navigation.setOptions({ title: name });
     
+    // anonymous authentication
     this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
       if (!user) {
         firebase.auth().signInAnonymously();
@@ -83,38 +84,43 @@ export default class Chat extends React.Component {
         text: data.text || "",
         user: {
           _id: data.user._id,
-          name: data.user.name,
-          avatar: data.user.avatar,
+          name: this.props.route.params.name,
+          avatar: 'https://placeimg.com/140/140/any',
           },
       });
     });
     this.setState({ messages });
   };
 
+  // updates state when message is sent
   onSend(messages = []) {
     this.setState((previousState) => ({
       messages: GiftedChat.append(previousState.messages, messages),
-    }));
+    }),
+    () => {
+      this.addMessage();
+    });
   }
 
+  // adds new message to database
   addMessage() {
     const message = this.state.messages[0];
     this.referenceMessages.add({
       _id: message._id,
-      uid: this.state.uid,
-      createdAt: message.createdAt.toDate(),
+      createdAt: message.createdAt,
       text: message.text,
       user: message.user,
     });
   }
 
+  // custom text bubble styling
   renderBubble(props) {
     return (
       <Bubble
         {...props}
         wrapperStyle={{
-          right: { backgroundColor: '#5060E0' }, 
-          left: { backgroundColor: '#F5F5F5' }
+          right: { backgroundColor: '#626262' }, 
+          left: { backgroundColor: '#EEE' }
         }}
       />
     )
@@ -130,7 +136,7 @@ export default class Chat extends React.Component {
           renderBubble={this.renderBubble.bind(this)}
           messages={this.state.messages}
           onSend={(messages) => this.onSend(messages)}
-          user={{ _id: 1, }}
+          user={this.state.user}
         />
         { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
         </ImageBackground>
@@ -142,15 +148,11 @@ export default class Chat extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    paddingTop: 40,     
+    justifyContent: 'center',    
   },
   theme: {
     flex: 1,
     width: "100%",
     flexDirection: "column",
   },
-  text: {
-    fontSize: 30,
-  }
 });
